9f336904a442d7bf6d018b991e0b0045224c6fb7
JustSoup321 <brandonboese@protonmail.com>
2023-08-10 13:19:11 -0400

    dt-bindings: reset: add dt binding header for MediaTek MT6762 resets

---
+++ include/dt-bindings/reset/mt6762-reset.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2022 Jami Kettunen
+ */
+
+#ifndef _DT_BINDINGS_RESET_CONTROLLER_MT6762
+#define _DT_BINDINGS_RESET_CONTROLLER_MT6762
+
+/* FIXME: Add other needed MT6762_TOPRGU_* #defines! */
+
+#define MT6762_TOPRGU_SW_RST_NUM               20
+
+#endif  /* _DT_BINDINGS_RESET_CONTROLLER_MT6763 */
